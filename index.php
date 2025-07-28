113a0a4bc45d5aab1694f9358914f402abca2a6b
johnkaz1 <74975675+johnkaz1@users.noreply.github.com>
2025-07-28 13:04:28 +0300

    Username & Phone Number
    Now the user can fill his name and phone number and sms will react accordingly

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <?php
+$message = '';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = 'johnkaz';
     $password = 'SMSposi!1';
     $from = urlencode('mysite');
-    $to = '306971745355';
-    $text = urlencode('Hello Giannis');
 
-    $url = "http://www.smsbox.gr/httpapi/sendsms.php?coding=UTF8&username=$username&password=$password&from=$from&to=$to&text=$text";
+    $to = isset($_POST['phone']) ? urlencode(trim($_POST['phone'])) : '';
+    $name = isset($_POST['user']) ? trim($_POST['user']) : '';
+    $text = urlencode("Hello " . $name);
 
-    $response = file_get_contents($url);
-    $message = "Απάντηση από server: " . htmlspecialchars($response);
+    if (!empty($to) && !empty($name)) {
+        $url = "http://www.smsbox.gr/httpapi/sendsms.php?coding=UTF8&username=$username&password=$password&from=$from&to=$to&text=$text";
+
+        $response = file_get_contents($url);
+        $message = "Απάντηση από server: " . htmlspecialchars($response);
+    } else {
+        $message = "Παρακαλώ εισάγετε όνομα και αριθμό τηλεφώνου.";
+    }
 }
 ?>
 
@@ -21,11 +29,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body>
     <h2>Αποστολή SMS με SMSBOX</h2>
+
     <?php if (!empty($message)): ?>
         <p><strong><?= $message ?></strong></p>
     <?php endif; ?>
+
     <form method="POST">
-        <button type="submit">Στείλε SMS στον Giannis</button>
+        <label for="user">Όνομα χρήστη:</label>
+        <input type="text" id="user" name="user" placeholder="Γράψτε όνομα" required>
+        <br><br>
+
+        <label for="phone">Αριθμός τηλεφώνου:</label>
+        <input type="text" id="phone" name="phone" placeholder="Γράψτε αριθμό τηλεφώνου" required>
+        <br><br>
+
+        <button type="submit">Στείλε SMS</button>
     </form>
 </body>
 </html>
